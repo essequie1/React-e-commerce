e2b40ce026e4ee8269440d185f83cc90445169be
Ezequiel <ezmartgod@gmail.com>
2023-04-13 04:25:18 -0300

    small change to wishlist

--- src/components/WishlistWidget.jsx
+++ src/components/WishlistWidget.jsx
@@ -2,15 +2,24 @@ import { useContext, useState } from 'react';
 import { userContext } from '../context/userContext';
 import { Wishlist } from './Wishlist';
 import '../scss/WishlistWidget.scss';
+import { toast } from 'react-toastify';
 
 const WishlistWidget = () => {
   const [isWishlistShown, setIsWishlistShown] = useState(false);
   const { userData } = useContext(userContext);
 
+  const handleShowWishlist = () => {
+    if (Object.keys(userData).length > 0) {
+      setIsWishlistShown(curr => !curr);
+    } else {
+      toast.warning('You have to log in to have a wishlist');
+    }
+  };
+
   return (
     <div className="wishlist-widget">
-      <button onClick={() => setIsWishlistShown(curr => !curr)}>WISHLIST</button>
-      {isWishlistShown ? <>{Object.keys(userData).length > 0 ? <Wishlist /> : <h1>You have to log in to see your wishlist</h1>}</> : null}
+      <button onClick={handleShowWishlist}>WISHLIST</button>
+      {isWishlistShown ? <Wishlist /> : null}
     </div>
   );
 };
